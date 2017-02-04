a85796918d143c87c49e8935ff05f2ba25f17372
Pascal J. Bourguignon <pjb@informatimago.com>
2017-02-05 00:45:04 +0100

    Deleted trailing whitespace.

--- solutions/sauts/sauts.lisp
+++ solutions/sauts/sauts.lisp
@@ -1,7 +1,7 @@
 ;; Jump!
 ;; February 3, 2017
 ;; Jump is a simple one-player game:
-;; 
+;;
 ;; You are initially at the first cell of an array of cells containing
 ;; non-negative integers; at each step you can jump ahead in the array as
 ;; far as the integer at the current cell, or any smaller number of
@@ -10,7 +10,7 @@
 ;; otherwise you lose. For instance, if the array contains the integers
 ;; [2, 0, 3, 5, 0, 0, 3, 0, 0, 3, 1, 0], you can win by jumping from 2,
 ;; to 3, to 5, to 3, to 3, then past the end of the array.
-;; 
+;;
 ;; Your task is to write a program that determines if a given game is
 ;; winnable. When you are finished, you are welcome to read or run a
 ;; suggested solution, or to post your own solution or discuss the
@@ -56,5 +56,3 @@
 (winnable-path #(1)) ; --> (1 t)
 (winnable-path #(0)) ; --> nil
 (winnable-path #())  ; --> (t)
-
-
